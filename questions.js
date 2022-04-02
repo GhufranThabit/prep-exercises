b5916596d93d325df32fca73ea510ce148184095
Ghufran <ghufranthabit@gmail.com>
2022-04-02 15:51:33 +0200

    my Questions for this week

---
+++ questions.js
@@ -0,0 +1,10 @@
+// Q1- I know what push() and shift() do, but what do the (Stand in Line)means?
+function nextInLine(arr, item) {
+  arr.push(item);
+  var removed = arr.shift();
+  return removed;
+}
+
+const testArr = [1, 2, 3, 4, 5];
+
+//Q2- what is the golf code in javascript means?
